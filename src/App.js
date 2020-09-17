95a1b20d8b8cef5160a768aa8c3c7d1ec09054cc
Bobae Kang <bkang.dev@gmail.com>
2020-09-17 13:16:04 -0500

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import IndexOverview from "./IndexOverview";
 
 function App() {
-  return <h1>Portal Overview</h1>;
+  const [token, setToken] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
+
+  return (
+    <div style={{ maxWidth: "500px" }}>
+      <label style={{ margin: "0 .25rem" }}>
+        Access Token:
+        <input
+          type="text"
+          style={{ margin: "0 .25rem" }}
+          onChange={(e) => setToken(e.target.value)}
+          value={token}
+        />
+      </label>
+      <button onClick={() => setIsFetching(true)}>Fetch</button>
+      <IndexOverview
+        isFetching={isFetching}
+        setIsFetching={setIsFetching}
+        token={token}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/IndexOverview.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+function fetchData({ token, query }) {
+  return fetch("https://portal.pedscommons.org/api/v0/submission/graphql/", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "*/*",
+      "Content-Type": "application/json",
+      Connection: "keep-alive",
+    },
+    body: JSON.stringify({ query }),
+  }).then((response) => response.json());
+}
+
+function IndexOverview({ isFetching, setIsFetching, token }) {
+  const [subject, setSubject] = useState(0);
+
+  useEffect(() => {
+    if (isFetching)
+      fetchData({ token, query: `{ _inrg_count }` })
+        .then(({ data }) => setSubject(data._inrg_count))
+        .catch((e) => console.error(e))
+        .finally(() => setIsFetching(false));
+  }, [isFetching]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        margin: "1rem .25rem",
+        padding: "1rem",
+      }}
+    >
+      <h1>Overview</h1>
+      <h2>Subjects: {subject}</h2>
+    </div>
+  );
+}
+
+export default IndexOverview;
